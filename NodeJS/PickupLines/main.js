5f79d09b2769e2f5dc7d6b014bd71f4bd025bad2
Bizzycola <furbanj@gmail.com>
2021-08-21 23:42:38 +1000

    Update main.js

--- NodeJS/PickupLines/main.js
+++ NodeJS/PickupLines/main.js
@@ -10,144 +10,145 @@ const port = 3000;
 const db = new jsoning("/dbdata/db.json");
 
 async function updateKeyCount(key) {
-  try {
-    if(await db.has(key))
-      await db.math(key, 'add', 1);
-    else
-      await db.set(key, 1);
-  } catch(err){ console.log(err); }
+    try {
+        if (await db.has(key))
+            await db.math(key, 'add', 1);
+        else
+            await db.set(key, 1);
+    } catch (err) { console.log(err); }
 }
 
 async function getKeyCount(key) {
-  var count = 0;
+    var count = 0;
 
-  if(await db.has(key))
-    count = parseInt(await db.get(key));
+    if (await db.has(key))
+        count = parseInt(await db.get(key));
 
-  return count;
+    return count;
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
-app.get('/stats', async (req, res) => {
-  res.send({
-    success: true,
-    pickupRequests: await getKeyCount('pickupLineRequests'),
-    putdownRequests: await getKeyCount('putdownLineRequests')
-  })
+app.get('/stats', async(req, res) => {
+    res.send({
+        success: true,
+        pickupRequests: await getKeyCount('pickupLineRequests'),
+        putdownRequests: await getKeyCount('putdownLineRequests')
+    })
 })
 
 app.get('/statsFile', (req, res) => {
-  res.sendFile('/dbdata/db.json')
+    res.sendFile('/dbdata/db.json')
 })
 
-app.get('/viewInitial', async (req, res) => {
-  await updateKeyCount('pickupLineRequests');
-  res.send({
-    success: true,
-    line: lineApi.getRandomPickupLine(),
-    numPickupLines: lineApi.getPickupLineCount(),
-    numPutdownLines: lineApi.getPutdownLineCount(),
-    pickupRequests: await getKeyCount('pickupLineRequests'),
-    putdownRequests: await getKeyCount('putdownLineRequests')
-  })
-})
-
-app.get('/dadjoke', async (req, res) => {
-  try {
-    var joke = await axios.get('https://icanhazdadjoke.com/', {
-      headers: {
-        "accept": 'text/plain',
-      },
+app.get('/viewInitial', async(req, res) => {
+    await updateKeyCount('pickupLineRequests');
+    res.send({
+        success: true,
+        line: lineApi.getRandomPickupLine(),
+        numPickupLines: lineApi.getPickupLineCount(),
+        numPutdownLines: lineApi.getPutdownLineCount(),
+        pickupRequests: await getKeyCount('pickupLineRequests'),
+        putdownRequests: await getKeyCount('putdownLineRequests')
     })
-    
-    res.send(joke.data)
-  } catch (err) {
-    console.log(err)
-    return res.send(false)
-  }
 })
 
-app.get('/line/random', async (req, res) => {
-  await updateKeyCount('pickupLineRequests');
+app.get('/dadjoke', async(req, res) => {
+    try {
+        var joke = await axios.get('https://icanhazdadjoke.com/', {
+            headers: {
+                "accept": 'text/plain',
+                "User-Agent": "PickupLines API (https://github.com/bizzycola)"
+            },
+        })
 
-  res.send({
-    success: true,
-    line: lineApi.getRandomPickupLine(),
-  });
+        res.send(joke.data)
+    } catch (err) {
+        console.log(err)
+        return res.send(false)
+    }
 })
-app.get('/nline/random', async (req, res) => {
-  await updateKeyCount('putdownLineRequests');
 
-  res.send({
-    success: true,
-    line: lineApi.getRandomPutdownLine(),
-  })
+app.get('/line/random', async(req, res) => {
+    await updateKeyCount('pickupLineRequests');
+
+    res.send({
+        success: true,
+        line: lineApi.getRandomPickupLine(),
+    });
+})
+app.get('/nline/random', async(req, res) => {
+    await updateKeyCount('putdownLineRequests');
+
+    res.send({
+        success: true,
+        line: lineApi.getRandomPutdownLine(),
+    })
 })
 
 app.get('/fact/random', (req, res) => {
-  res.send({
-    success: true,
-    line: lineApi.getRandomFact()
-  })
+    res.send({
+        success: true,
+        line: lineApi.getRandomFact()
+    })
 })
 app.get('/text/fact/random', (req, res) => {
-  res.send(lineApi.getRandomFact().line);
+    res.send(lineApi.getRandomFact().line);
 })
 
-app.get('/text/line/random', async (req, res) => {
-  await updateKeyCount('pickupLineRequests');
+app.get('/text/line/random', async(req, res) => {
+    await updateKeyCount('pickupLineRequests');
 
-  res.send(lineApi.getRandomPickupLine().line)
+    res.send(lineApi.getRandomPickupLine().line)
 })
-app.get('/text/nline/random', async (req, res) => {
-  await updateKeyCount('putdownLineRequests');
+app.get('/text/nline/random', async(req, res) => {
+    await updateKeyCount('putdownLineRequests');
 
-  res.send(lineApi.getRandomPutdownLine().line)
+    res.send(lineApi.getRandomPutdownLine().line)
 })
 
-app.get('/line/:id', async (req, res) => {
-  await updateKeyCount('pickupLineRequests');
-  res.send({
-    success: true,
-    line: lineApi.getPickupLineById(req.params.id),
-  })
+app.get('/line/:id', async(req, res) => {
+    await updateKeyCount('pickupLineRequests');
+    res.send({
+        success: true,
+        line: lineApi.getPickupLineById(req.params.id),
+    })
 })
-app.get('/text/line/:id', async (req, res) => {
-  await updateKeyCount('pickupLineRequests');
-  res.send(lineApi.getPickupLineById(req.params.id).line)
+app.get('/text/line/:id', async(req, res) => {
+    await updateKeyCount('pickupLineRequests');
+    res.send(lineApi.getPickupLineById(req.params.id).line)
 })
 
 app.get('/text/pun/random', (req, res) => {
-  res.send(lineApi.getRandomPun().line)
+    res.send(lineApi.getRandomPun().line)
 })
 app.get('/pun/random', (req, res) => {
-  res.send({
-    success: true,
-    line: lineApi.getRandomPun()
-  })
+    res.send({
+        success: true,
+        line: lineApi.getRandomPun()
+    })
 })
 
 app.get('/lines', (req, res) => {
-  let page = req.query.page ? req.query.page : 1
-  res.send({
-    success: true,
-    lines: lineApi.getPickupLines(page),
-  })
+    let page = req.query.page ? req.query.page : 1
+    res.send({
+        success: true,
+        lines: lineApi.getPickupLines(page),
+    })
 })
 
 app.get('/nlines', (req, res) => {
-  let page = req.query.page ? req.query.page : 1
-  res.send({
-    success: true,
-    lines: lineApi.getPutdownLines(page),
-  })
+    let page = req.query.page ? req.query.page : 1
+    res.send({
+        success: true,
+        lines: lineApi.getPutdownLines(page),
+    })
 })
 
 app.listen(port, () => {
-  console.log(`Pickup line API listening at http://localhost:${port}`)
+    console.log(`Pickup line API listening at http://localhost:${port}`)
 })
 
 // Test commit 7
